f93dd7d38d5a753ce7d56a563b78f7a2b9c671fa
Modester-mwa <70933725+Modester-mw@users.noreply.github.com>
2020-09-12 17:07:48 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,48 @@
+Question 2
+In plain English and with the Given-required-algorithm table write a guessing game where the user has to guess a secret number. After every guess the program tells the user whether their number was too large or too small. At the end the number of tries needed should be printed. It counts only as one try if they input the same number multiple times consecutively.
+
+Given
+
+We are writing a program for a number guessing game.
+
+The secret number is an integer.
+
+The program displays a message after every guess saying whether the number was too large or too small.
+
+The program counts and records the number of unique guesses made.
+
+The program congratulates the user once the secret number is guessed correclty.
+
+Required
+Write a guessing game(program).
+Guess a secret number for such as”4”.
+
+To write a program that tells the user whether the number guessed is too large or too small.
+
+Write a program that prints the number of tries made..
+
+Create the program such that it  counts one try when the user guesses the same number multiple times consecutively.
+
+Algorithm
+Let the range where the secret number lies be  from 1 to 50.
+
+We guess the value “15” as the secret number.
+
+Assign the value 15 to variable X such that X = 15.
+
+The user requested to guess the number from 1 to 50.
+
+If the # guessed is <15 the program tells the user the number is “too small”.
+
+If the user guesses a #  >15 the program says the number is “too large”.
+
+If the user guesses X == 15 then the program congratulates the user for winning the game.
+
+When a user wins the game, the program counts the number of tries made and prints them.
+
+The game is over once the user guesses the number correctly.
+
+
+
+
+
